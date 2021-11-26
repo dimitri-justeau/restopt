c221aead684abec063b45e833bc27b492b5c2bf7
Dimitri <dimitri.justeau@gmail.com>
2021-11-26 16:04:25 +1100

    Fix test having absolute paths

--- src/test/java/org/restopt/TestSolve.java
+++ src/test/java/org/restopt/TestSolve.java
@@ -4,6 +4,8 @@ import org.chocosolver.solver.exception.ContradictionException;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class TestSolve {
 
@@ -13,11 +15,28 @@ public class TestSolve {
         String restorable = getClass().getClassLoader().getResource("example_data/restorable.tif").getPath();
         String accessible = getClass().getClassLoader().getResource("example_data/accessible.tif").getPath();
         DataLoader dataLoader = new DataLoader(habitat, accessible, restorable);
+        BaseProblem baseProblem = new BaseProblem(dataLoader, 1);
+        baseProblem.postNbComponentsConstraint(1, 1);
+        baseProblem.postCompactnessConstraint(6);
+        baseProblem.postRestorableConstraint(90, 110, 23, 0.7);
+        Path temp = Files.createTempFile("test_mesh", "");
+        baseProblem.maximizeMESH(3, temp.toString(), 0, false);
+        Files.delete(temp);
+    }
+
+    @Test
+    public void testSolveMESH2() throws IOException, ContradictionException {
+        String habitat = getClass().getClassLoader().getResource("example_data/habitat.tif").getPath();
+        String restorable = getClass().getClassLoader().getResource("example_data/restorable.tif").getPath();
+        String accessible = getClass().getClassLoader().getResource("example_data/accessible.tif").getPath();
+        DataLoader dataLoader = new DataLoader(habitat, accessible, restorable);
         BaseProblem baseProblem = new BaseProblem(dataLoader, 2);
         baseProblem.postNbComponentsConstraint(1, 1);
         baseProblem.postCompactnessConstraint(6);
         baseProblem.postRestorableConstraint(90, 110, 23, 0.7);
-        baseProblem.maximizeMESH(3, "/home/djusteau/restopt2", 0, false);
+        Path temp = Files.createTempFile("test_mesh2", "");
+        baseProblem.maximizeMESH(3, temp.toString(), 0, false);
+        Files.delete(temp);
     }
 
     @Test
@@ -30,7 +49,9 @@ public class TestSolve {
         baseProblem.postNbComponentsConstraint(1, 1);
         baseProblem.postCompactnessConstraint(6);
         baseProblem.postRestorableConstraint(90, 110, 23, 0.7);
-        baseProblem.maximizeIIC(3, "/home/djusteau/restopt", 0, false);
+        Path temp = Files.createTempFile("test_iic", "");
+        baseProblem.maximizeIIC(3, temp.toString(), 0, false);
+        Files.delete(temp);
     }
 
     @Test
@@ -43,6 +64,8 @@ public class TestSolve {
         baseProblem.postNbComponentsConstraint(1, 1);
         baseProblem.postCompactnessConstraint(6);
         baseProblem.postRestorableConstraint(90, 110, 23, 0.7);
-        baseProblem.maximizeIIC(3, "/home/djusteau/restopt", 0, false);
+        Path temp = Files.createTempFile("test_iic2", "");
+        baseProblem.maximizeIIC(3, temp.toString(), 0, false);
+        Files.delete(temp);
     }
 }
